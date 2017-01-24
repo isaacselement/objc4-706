f6f3b62ea7b85c610e4493f529d49a9b980fbde9
Man In The Middle <isaacselement@gmail.com>
2017-01-24 16:52:35 +0800

    add debug-objc target

---
+++ debug-objc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  debug-objc
+//
+//  Created by suchavision on 1/24/17.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
